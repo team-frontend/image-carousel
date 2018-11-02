71ad8e29057a714a32e60135417dd017bbff21b7
Chih-Lin Lee <mlee749@gmail.com>
2018-11-01 23:12:40 -0700

    carousel scroll right working, left is having cross origin error

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -3,6 +3,12 @@ import RightArrow from './RightArrow.jsx';
 import LeftArrow from './LeftArrow.jsx';
 import Images from './Images.jsx';
 import Image from './Image.jsx';
+import { relative } from 'path';
+
+const stringPxToNum = (string) => {
+  let num = string.split('px');
+  return Number(num[0]);
+};
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -12,6 +18,10 @@ class Carousel extends React.Component {
       currentIndex: 0,
       toggle: false,
       image: [],
+      viewStyle : {
+        position: "relative",
+        right: "0px"
+      },
     };
     this.renderImage = this.renderImage.bind(this);
     this.goBack = this.goBack.bind(this);
@@ -38,7 +48,12 @@ class Carousel extends React.Component {
         image: this.state.images[this.state.currentIndex - 1].imageUrl,
       });
     } else {
-      console.log('move carousel back');
+      this.setState({
+        viewStyle: {
+          position: "relative",
+          right: stringPxToNum(this.state.viewStyle.right) - 100 + "px"
+        },
+      });
     }
     // need to do if else to check if carousel or selected image is shown. If carousel, the arrows need to move differently than if 1 image
   }
@@ -51,7 +66,13 @@ class Carousel extends React.Component {
         image: this.state.images[Number(this.state.currentIndex) + 1].imageUrl,
       });
     } else {
-      console.log('move carousel forward');
+      this.setState({
+        viewStyle: {
+          position: "relative",
+          right: stringPxToNum(this.state.viewStyle.right) + 100 + "px"
+        },
+      });
+      // console.log(stringPxToNum(this.state.viewStyle.right) + 100 + "px")
     }
     // need to do if else to check if carousel or selected image is shown. If carousel, the arrows need to move differently than if 1 image
   }
@@ -84,16 +105,20 @@ class Carousel extends React.Component {
     );
   }
 
+
   render() {
     console.log('this is images state', this.state.images);
     console.log('this is image state', this.state.image)
+    console.log('this is viewStyle', this.state.viewStyle)
     return (
       <div>
-        <LeftArrow goBack={this.goBack} />
-        <RightArrow goForward={this.goForward} />
-        {this.state.toggle ? this.renderSelectedImage(): this.renderCarousel()}
-        {/* <Image image={this.state.image} />
-        <Images images={this.state.images} renderImage={this.renderImage} /> */}
+          <LeftArrow goBack={this.goBack} />
+          <RightArrow goForward={this.goForward} />
+        <div style={this.state.viewStyle}>
+          {this.state.toggle ? this.renderSelectedImage(): this.renderCarousel()}
+          {/* <Image image={this.state.image} />
+          <Images images={this.state.images} renderImage={this.renderImage} /> */}
+        </div>
       </div>
     );
   }
